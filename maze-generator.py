6bd485eb3a2a661e2135cf8951b121e0ae5a7b61
Sean Yager <contact@seanyager.com>
2022-12-28 12:37:34 -0500

    python GUI!

--- maze-generator.py
+++ maze-generator.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python
+from tkinter import *
+root = Tk()
 
-import tkinter
-top = tkinter.Tk()
-# Code to add widgets will go here...
-top.mainloop()
-name = "eli"
+Label(text="Position 1", width=10).grid(row=0, column=0)
+Label(text="Position 2", width=10).grid(row=0, column=1)
+Label(text="Position 3", width=10).grid(row=1, column=0)
+Label(text="Position 4", width=10).grid(row=1, column=1)
+root.mainloop()
